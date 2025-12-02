cdf807b6fda0aa86d59aac2604303393cdf75488
Gytisla <glazaitis@gmail.com>
2025-12-02 21:57:13 +0100

    add base path

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/esphome-lvgl-designer/',
   plugins: [
     vue(),
     tailwindcss(),
